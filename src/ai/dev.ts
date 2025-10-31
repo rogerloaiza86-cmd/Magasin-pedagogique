76c1dcd80c467fb133be8731d4ed00cabbea856e
Roger Loaiza <roger.loaiza86@gmail.com>
2025-10-31 13:04:37 +0000

    Try fixing this error: `Console Error: Error: Failed to fetch from https

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,6 +1,10 @@
+
 'use server';
 import { config } from 'dotenv';
 config();
+
+// This must be imported before any other flows are.
+import '@/ai/genkit';
 
 import '@/ai/flows/optimize-picking-route.ts';
 import '@/ai/flows/document-generation-assistance.ts';
