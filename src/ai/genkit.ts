fed9512ee7b3c7964d21bb924fc59f58ad5348a5
Roger Loaiza <roger.loaiza86@gmail.com>
2025-10-31 13:07:02 +0000

    Try fixing this error: `Console Error: Error: Failed to fetch from https

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -5,6 +5,6 @@ import {config} from 'dotenv';
 config();
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
+  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY || ' '})],
   model: 'googleai/gemini-2.5-flash',
 });
